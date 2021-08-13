510955274780ea295743d649e2733c723739e727
Mikel Granero <mikelgmh@gmail.com>
2021-08-13 09:59:08 +0200

    Eliminado jquery completamente, eliminados archivos innecesarios

--- script.js
+++ script.js
@@ -1,81 +1,107 @@
-$(document).ready(function () {
-    function getLineas() {
+    async function getLineas() {
         var lineas = [];
-        jQuery.ajax({
-            url: "https://cors.bridged.cc/https://www.bilbao.eus/cs/Satellite/bilbobus/es/lineas-bilbobus/mapa-de-lineas?temporada_linea=VE&codLinea=22",
-            method: "GET",
+        const res = await axios.get('https://cors.bridged.cc/https://www.bilbao.eus/cs/Satellite/bilbobus/es/lineas-bilbobus/mapa-de-lineas?temporada_linea=VE&codLinea=22', {
             headers: {
                 "Access-Control-Allow-Headers": [],
                 "x-requested-With": "XMLHttpRequest",
-                "x-requested-by": undefined
             }
-        }).then(response => {
-            var elems = $(response).find("table.tparadas"); // 3 tablas, la info está en la 2 y 3
-            elems.each(function () {
-                var elementos = $(this).find("tbody").find("tr");
-                elementos.each(function () {
-
-                    var otros = $(this).find("td").find("a");
-                    otros.each(function () {
-
-                        // Se imprimen los nombres de todas las líneas
-                        var lineaString = $(this).text();
-                        const parts = lineaString.split(":");
-                        if (parts[1] != undefined) {
-
-                            lineas.push({
-                                numLinea: parts[0],
-                                nombre: parts[1].trim(),
-                            })
-                        }
-
-                    });
+        });
+        parser = new DOMParser();
+        var elems = parser.parseFromString(res.data, "text/html");
+        elems = elems.querySelectorAll("table.tparadas")
+        elems.forEach(element => {
+            var tr = element.querySelectorAll("tbody tr")
+            tr.forEach(fila => {
+                var otros = fila.querySelectorAll("td a");
+                otros.forEach(otro => {
+                    // Se imprimen los nombres de todas las líneas
+                    var lineaString = otro.textContent;
+                    const parts = lineaString.split(":");
+                    if (parts[1] != undefined) {
+                        lineas.push({
+                            numLinea: parts[0],
+                            nombre: parts[1].trim(),
+                        })
+                    }
                 });
             });
-            console.log("------ LINEAS ------")
-            console.log(lineas)
-        }).catch(error => {
-            console.log(error);
-        })
+        });
+        console.log(lineas)
+        return lineas;
     }
 
-    function getTiempos() {
+    async function getTiempos() {
         var tiemposByParada = [];
-        jQuery.ajax({
-            url: "https://cors.bridged.cc/https://www.bilbao.eus/cs/Satellite/bilbobus/es/linea?temporada_linea=VE&codLinea=01&Trayecto1=1",
-            method: "GET",
+        const response = await axios.get('https://cors.bridged.cc/https://www.bilbao.eus/cs/Satellite/bilbobus/es/linea?temporada_linea=VE&codLinea=01&Trayecto1=1', {
             headers: {
-                "Access-Control-Allow-Headers": [],
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
                 "x-requested-With": "XMLHttpRequest",
-                "x-requested-by": undefined
-    
             }
-        }).then(response => {
-            var table = $(response).find("table");
-            var tiemposFila = $(table[1]).find("tbody").find("tr");
-            tiemposFila.each(function () {
-                var hrefParada = $(this).find("[headers='horas_de_paso_ida']").find("a").attr('href');
+        });
+        parser = new DOMParser();
+        var htmlData = parser.parseFromString(response.data, "text/html");
+        var tables = htmlData.querySelectorAll("table")
+        var tiemposFila = tables[1].querySelectorAll("tbody tr");
+
+        tiemposFila.forEach(element => {
+            var hrefParada = element.querySelectorAll("[headers='horas_de_paso_ida'] a");
+            hrefParada.forEach(elemento => {
+                var href = elemento.href;
                 var Y = "parada="
-                var codParada = parseInt(hrefParada.split(Y).pop());
+                var codParada = parseInt(href.split(Y).pop());
                 var infoParada = {
-                    tiempo: $(this).find("[headers='tiempoespera_ida']").text().replace("'", ''),
-                    parada: $(this).find("[headers='parada_ida']").text(),
-                    zona: $(this).find("[headers='zona_ida']").text(),
+                    tiempo: element.querySelectorAll("[headers='tiempoespera_ida']")[0].textContent.replace("'", ''),
+                    parada: element.querySelectorAll("[headers='parada_ida']")[0].textContent.trim(),
+                    zona: element.querySelectorAll("[headers='zona_ida']")[0].textContent,
                     numparada: codParada,
                 };
-                if(infoParada.tiempo == ""){
-                   var llegando = $(this).find("[headers='tiempoespera_ida']").find("[title='Llegando']");
-                   if(llegando.length >0){
-                       infoParada.tiempo = "LLEGANDO";
-                   }
+                if (infoParada.tiempo == "") {
+                    var llegando = element.querySelectorAll("[headers='tiempoespera_ida'] [title='Llegando']");
+                    if (llegando.length > 0) {
+                        infoParada.tiempo = "LLEGANDO";
+                    }
                 }
-                tiemposByParada.push(infoParada)
-            })
-            console.log("------ TIEMPOS PARADAS ------")
-            console.log(tiemposByParada);
-        })
+                if(!containsObject(infoParada,tiemposByParada)){
+                    tiemposByParada.push(infoParada)
+                }
+            });
+        });
+        console.log(tiemposByParada)
+        return tiemposByParada;
     }
-    // getLineas();
-    getTiempos();
-  
-});
+
+    async function getTiemposParada(parada){
+        const response = await axios.get('https://cors.bridged.cc/https://www.bilbao.eus/cs/Satellite/bilbobus/es/horario?codLinea=01&temporada=VE', {
+            headers: {
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+                "x-requested-With": "XMLHttpRequest",
+            },
+            params:{
+                parada: parada
+            }
+        });
+        parser = new DOMParser();
+        var htmlData = parser.parseFromString(response.data, "text/html");
+        var tables = htmlData.querySelectorAll("table")
+        var tiemposFila = tables[0].querySelectorAll("tbody tr");
+        var tiempos = tiemposFila[1];
+        var minutos = tiempos.querySelectorAll("td")[0].textContent.replace("'", '');
+        console.log(minutos)
+        return minutos;
+    }
+
+    function containsObject(obj, list) {
+        var i;
+        for (i = 0; i < list.length; i++) {
+            if (list[i].numparada == obj.numparada) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+    //getLineas();
+    //getTiempos();
+    getTiemposParada(1604);
